2cb8b12dc48cc98ec7384f1c03aa7533efb4a9e0
venkat <devopsaws84s@gmail.com>
2026-02-06 15:04:20 +0530

    Terraform

--- main.tf
+++ main.tf
@@ -1,7 +1,12 @@
 module "vpc" {
     source = "../Terraform-Module-VPC"
-    project = "Roboshop"
-    environment = "Dev"
-    public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24"]
-  
+   # project = "Roboshop"
+    #environment = "Dev"
+    #public_subnet_cidr = ["10.0.1.0/24", "10.0.2.0/24"]
+    
+    project = var.Project
+    environment = var.Environment
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr
+    database_subnet_cidr = var.database_subnet_cidr
 }
