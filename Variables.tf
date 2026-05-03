d8051d22184467b50e74ac0bcfab05dd81f2e2cf
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-05-03 15:18:28 +0530

    Terraform MOdules

--- Variables.tf
+++ Variables.tf
@@ -5,3 +5,7 @@ variable "project" {
 variable "environment"{
     default = "Dev"
 }
+
+variable "publicsubnet_cidr" {
+    default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
--- main.tf
+++ main.tf
@@ -2,5 +2,6 @@ module "vpc" {
     source = "../Terraform-Module-VPC"
     project = var.project
     environment = var.environment
+    public_subnet_cidr = var.publicsubnet_cidr
   
 }
