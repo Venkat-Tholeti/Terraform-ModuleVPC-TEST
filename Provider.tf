1b49f515e921a8fb035c82f6fd22a3a7b7ba963c
Venkat-Tholeti <naidutholeti12@gmail.com>
2026-05-04 17:40:33 +0530

    Terraform Module

--- Provider.tf
+++ Provider.tf
@@ -8,7 +8,7 @@ terraform {
 
     backend "s3" {
         bucket         = "my-terraform-state-bucket-vt" 
-        key            = "VPC-demo-test"
+        key            = "VPC-demo"
         region         = "us-east-1"
         encrypt        = true
         use_lockfile   =  true
